fee9571b29cc7cbcea2daf176b66a7c21f042ff1
senthilraj <senthil2rajan@gmail.com>
2014-09-03 15:20:21 +0530

    new

--- js/slidejs.js
+++ js/slidejs.js
@@ -3,7 +3,8 @@
 	$.fn.slidejs = function(options) {
 		var defaults = {
 			speed : 5000,
-			slide_speed : 600
+			slide_speed : 600,
+                        effect : "normal"
 		};
 		
 		var settings = $.extend( {}, defaults, options );
@@ -15,21 +16,78 @@
 			console.log(img_wid);
 			console.log(img_len);
 			var parent_wid = img_wid * img_len;
-			ele.find("ul").css({"width":parent_wid+"px", "position":"relative", "left":"0px"});
 			var count=1;
-			setInterval(function(){
-				//alert();
-				if(count<img_len)
-				{
-				ele.find("ul").animate( { left: "-"+count*img_wid+"px"}, settings.slide_speed);
-				
-				count++;
-				}
-				else{
-					count = 0;
-				}
-			
-			}, settings.speed);
+                        if(settings.effect == "normal")
+                            {
+                                ele.find("ul").css({"width":parent_wid+"px", "position":"relative", "left":"0px"});
+                                setInterval(function(){
+                                        //alert(); 
+                                        if(count<img_len)
+                                        {
+                                        ele.find("ul").animate( { left: "-"+count*img_wid+"px"}, settings.slide_speed);
+
+                                        count++;
+                                        }
+                                        else{
+                                                count = 0;
+                                        }
+
+                                }, settings.speed);
+                            }
+                        else if(settings.effect == "swing")
+                        {
+                            
+                            ele.find("ul").css({"position":"relative", "width": img_wid+"px", "perspective" : img_wid+"px",});
+                            ele.find("ul img").hide();
+                            var count = 0;
+                            var count_inc = 0;
+                            ele.find("ul img").eq(count).show();
+                            setInterval(function(){
+                                    //alert();
+                                    count_inc = count+1;
+                                if(count<img_len-1)
+                                {
+                                ele.find("ul img").hide();
+                                ele.find("ul img").removeClass();
+                                ele.find("ul img").eq(count).show();
+                                ele.find("ul img").eq(count).addClass("swing");
+                                ele.find("ul img").eq(count).addClass("normal");
+                                setTimeout(function(){
+                                    console.log(count);
+                                    ele.find("ul img").eq(count_inc).addClass("normal2");
+                                    ele.find("ul img").eq(count_inc).show();
+                                    ele.find("ul img").eq(count).hide();
+                                    ele.find("ul img").eq(count_inc).addClass("swing2");
+                                    count++;
+                                },300);
+                                
+                                }
+                                    else{
+                                        console.log(count_inc);
+                                        count = 0;
+                                        ele.find("ul img").eq(count).addClass("normal2");
+                                        ele.find("ul img").eq(count).show();
+                                        ele.find("ul img").eq(count_inc-1).hide();
+                                        ele.find("ul img").eq(count).addClass("swing2");
+                                                
+                                                
+                                        }
+                                /*setTimeout(function(){
+                                    ele.find("ul img").eq(0).removeClass();
+                                     ele.find("ul img").eq(1).removeClass("normal2");
+                                   //alerT();
+                                   // ele.find("ul img").eq(1).addClass("swing");
+                                    // ele.find("ul img").eq(1).addClass("normal");
+                                    setTimeout(function(){
+                                        ele.find("ul img").eq(1).hide();
+                                        ele.find("ul img").eq(0).addClass("swing");
+                                        ele.find("ul img").eq(0).show();
+                                        ele.find("ul img").eq(0).addClass("normal");
+                                     },400);
+                                },3500);*/
+
+                                }, settings.speed);
+                        }
 		});
 	
 	};
